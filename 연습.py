1af16897e67897b984fe32215e413d5ebf8d762a
Donghyeon Yun <sadmander91@Donghyeonui-MacBookPro.local>
2026-04-17 15:00:47 +0900

    initial commit

--- 연습.py
+++ 연습.py
@@ -4,3 +4,10 @@ arr = array.replace(array[1], 'y')
 
 print(array)
 print(arr)
+
+
+git init
+git add .
+git commit -am "initial commit"
+git remote add origin https://github.com/sadmander91/test.git
+git push origin master
