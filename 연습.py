a260fd3b328219ab372829e0f9371903d687da4b
Donghyeon Yun <sadmander91@Donghyeonui-MacBookPro.local>
2026-04-17 16:46:35 +0900

    commit

--- 연습.py
+++ 연습.py
@@ -5,5 +5,30 @@ arr = array.replace(array[1], 'y')
 print(array)
 print(arr)
 
-git remote remove origin
-git remote add origin https://
+
+
+
+# # remove existing remote
+# git remote remove origin
+
+# # add new remote
+# git remote add origin https://
+
+# # rename current branch to main
+# git branch -M main
+
+# # stage changes
+# git add .
+
+# # commit changes
+# git coommit -m "message"
+
+# # push and set upstream
+# git push -u origin main
+
+
+
+# # daily flow
+# git add .
+# git commit "message"
+# git push
